8cff47cc2b47c607f772f1989c1fae504f845f46
Prasad <prasad.09539@gmail.com>
2017-08-17 19:06:41 +0530

    null pointer issue

--- initproject/src/main/java/com/vsn/initproject/LibraryConfig.java
+++ initproject/src/main/java/com/vsn/initproject/LibraryConfig.java
@@ -4,7 +4,7 @@ package com.vsn.initproject;
  * Created by prasad on 16/08/17.
  */
 
-public class LibraryConfig {
-    public static final String oAuthKey = "��1_��\u0018\u0011)G�)����";
-    public static boolean isVerified = false;
+class LibraryConfig {
+    static final String oAuthKey = "��1_��\u0018\u0011)G�)����";
+    static boolean isVerified = false;
 }
